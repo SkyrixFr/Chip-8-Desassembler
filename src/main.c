88c200774549802b8449ae446fcc81b7db29b9ea
SkyrixFr <63282721+SkyrixFr@users.noreply.github.com>
2023-01-02 21:58:42 +0100

    02/01/2023 ajouter des descs

--- src/main.c
+++ src/main.c
@@ -24,12 +24,12 @@ void disassembleRomChip8(uint8_t *codebuffer, int16_t programCounter) {
                 {
                     case 0x00:
                     {
-                        printf("CLS");
+                        printf("%-25s %s", "CLS", "Clears screen");
                     }
                     break;
                     case 0x0e:
                     {
-                        printf("RET");
+                        printf("%-25s %s", "RET", "Return from a subroutine");
                     }
                     break;
                 }
@@ -45,37 +45,37 @@ void disassembleRomChip8(uint8_t *codebuffer, int16_t programCounter) {
         break;
         case 0x01:
         {
-            printf("%-10s 0x0%01x%02x", "JMP", secondnib, code[1]);
+            printf("%-10s 0x0%01x%02x %-8s%s%01x%02x", "JMP", secondnib, code[1], "", "Jump to memory location 0x0", secondnib, code[1]);
         }
         break;
         case 0x02:
         {
-            printf("%-10s #$%01x%02x", "CALL", secondnib, code[1]);
+            printf("%-10s 0x0%01x%02x %-8s%s%01x%02x", "CALL", secondnib, code[1], "", "Call subroutine at address 0x0", secondnib, code[1]);
         }
         break;
         case 0x03:
         {
-            printf("%-10s V%01x,0x%02x", "CMP", secondnib, code[1]);
+            printf("%-10s V%01x,0x%02x %-7s%s%01x %s%02x", "CMP", secondnib, code[1], "", "Skip next instruction if V", secondnib, "is equal to 0x", code[1]);
         }
         break;
         case 0x04:
         {
-            printf("%-10s V%01x,0x%02x", "CMPN", secondnib, code[1]);
+            printf("%-10s V%01x,0x%02x %-7s%s%01x %s%02x", "CMPN", secondnib, code[1], "", "Skip next instruction if V", secondnib, "is not equal to 0x", code[1]);
         }
         break;
         case 0x05:
         {
-            printf("%-10s V%01x,V%01x", "CMP", secondnib, thirdnib);
+            printf("%-10s V%01x,V%01x %-7s%s%01x %s%01x", "CMP", secondnib, thirdnib, "", "Skip next instruction if V", secondnib, "is equal to V", thirdnib);
         }
         break;
         case 0x06:
         {
-            printf("%-10s V%01X,0x%02x", "SET", secondnib, code[1]);
+            printf("%-10s V%01X,0x%02x %-7s%s%01x %s%02x", "SET", secondnib, code[1], "", "Set V", secondnib, "to 0x", code[1]);
         }
         break;
         case 0x07:
         {
-            printf("%-10s V%01X,0x%02x", "ADD", secondnib, code[1]);
+            printf("%-10s V%01X,0x%02x %-7s%s%01x %s%01x %s%02x", "ADD", secondnib, code[1], "", "Set V", secondnib, "= V", secondnib, "+ 0x", code[1]);
         }
         break;
         case 0x08:
@@ -84,27 +84,27 @@ void disassembleRomChip8(uint8_t *codebuffer, int16_t programCounter) {
             {
                 case 0x00:
                 {
-                    printf("%-10s V%01X,V%01x", "MOV", secondnib, thirdnib);
+                    printf("%-10s V%01X,V%01x %-9s%s%01x %s%01x", "SET", secondnib, thirdnib, "", "Set V", secondnib, "= V", thirdnib);
                 }
                 break;
                 case 0x01:
                 {
-                    printf("%-10s V%01X,V%01x", "OR", secondnib, thirdnib);
+                    printf("%-10s V%01X,V%01x %-9s%s%01x %s%01x", "OR", secondnib, thirdnib, "", "Performes a bitwise OR on V", secondnib, "and V", thirdnib);
                 }
                 break;
                 case 0x02:
                 {
-                    printf("%-10s V%01X,V%01x", "AND", secondnib, thirdnib);
+                    printf("%-10s V%01X,V%01x %-9s%s%01x %s%01x", "AND", secondnib, thirdnib, "", "Performes a bitwise AND on V", secondnib, "and V", thirdnib);
                 }
                 break;
                 case 0x03:
                 {
-                    printf("%-10s V%01X,V%01x", "XOR", secondnib, thirdnib);
+                    printf("%-10s V%01X,V%01x %-9s%s%01x %s%01x", "XOR", secondnib, thirdnib, "", "Performes a bitwise XOR on V", secondnib, "and V", thirdnib);
                 }
                 break;
                 case 0x04:
                 {
-                    printf("%-10s V%01X,V%01x", "ADD", secondnib, thirdnib);
+                    printf("%-10s V%01X,V%01x %-9s%s%01x %s%01x %s%01x %s", "ADD", secondnib, thirdnib, "", "V", secondnib, "= V", secondnib, "+ V", thirdnib, "and puts the carry in VF");
                 }
                 break;
                 case 0x05:
@@ -156,8 +156,89 @@ void disassembleRomChip8(uint8_t *codebuffer, int16_t programCounter) {
             printf("%-10s V%01X,V%01x,%01x", "DRW", secondnib, thirdnib, fourthnib);
         }
         break;
-        case 0x0e: printf("e not handled yet"); break;
-        case 0x0f: printf("f not handled yet"); break;
+        case 0x0e:
+        {
+            switch(thirdnib)
+            {
+                case 0x09:
+                {
+                    printf("%-10s V%01X", "SKP", secondnib);
+                }
+                break;
+                case 0x0a:
+                {
+                    printf("%-10s V%01X", "SKNP", secondnib);
+                }
+                break;
+            }
+        }
+        break;
+        case 0x0f:
+        {
+            switch(thirdnib)
+            {
+                case 0x00:
+                {
+                    switch(fourthnib)
+                    {
+                        case 0x07:
+                        {
+                            printf("%-10s V%01X,DT", "LD", secondnib);
+                        }
+                        break;
+                        case 0x0a:
+                        {
+                            printf("%-10s V%01X,K", "LD", secondnib);
+                        }
+                        break;
+                    }
+                }
+                break;
+                case 0x01:
+                {
+                    switch(fourthnib)
+                    {
+                        case 0x05:
+                        {
+                            printf("%-10s DT,V%01X", "LD", secondnib);
+                        }
+                        break;
+                        case 0x08:
+                        {
+                            printf("%-10s ST,V%01X", "LD", secondnib);
+                        }
+                        break;
+                        case 0x0e:
+                        {
+                            printf("%-10s I,V%01X", "ADD", secondnib);
+                        }
+                        break;
+                    }
+                }
+                break;
+                case 0x02:
+                {
+                    printf("%-10s F,V%01X", "LD", secondnib);
+                }
+                break;
+                case 0x03:
+                {
+                    printf("%-10s B,V%01X", "LD", secondnib);
+                }
+                break;
+                case 0x05:
+                {
+                    printf("%-10s [I],V%01X", "LD", secondnib);
+                }
+                break;
+                case 0x06:
+                {
+                    printf("%-10s V%01X,[I]", "LD", secondnib);
+                }
+                break;
+            }
+        }
+        break;
     }
 }
 
